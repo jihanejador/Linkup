f230a4a289669e9b325eef6f3c0e81adbd21af52
jihanejador <jihanejador@gmail.com>
2026-06-25 17:11:51 +0100

    creation les attrubes de table posts

--- linkup/database/migrations/2026_06_25_160026_create_posts_table.php
+++ linkup/database/migrations/2026_06_25_160026_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreinId('user_id')
+            ->constrained()
+            ->onDelete('cascade');
+            $table->text('content');
             $table->timestamps();
         });
     }
