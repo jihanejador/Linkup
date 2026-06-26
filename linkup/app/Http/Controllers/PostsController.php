35fddac14703b2b2cf98fc6a35bb573718f6bb84
jihanejador <jihanejador@gmail.com>
2026-06-26 11:33:23 +0100

    creation la function de l' affichage

--- linkup/app/Http/Controllers/PostsController.php
+++ linkup/app/Http/Controllers/PostsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post ::with('user')->latest()->get();
+        return view('feed', compact('posts'));
+    }
 }
