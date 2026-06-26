bb94f36c1cdb7f9c15f782fa8694a28f12d8b162
jihanejador <jihanejador@gmail.com>
2026-06-26 11:42:16 +0100

    creation le route

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/feed', [PostsController::class, 'index']);
