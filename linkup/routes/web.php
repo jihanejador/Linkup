63343bcf6706b5cb9cff07d81a612d9e45d86f6c
jihanejador <jihanejador@gmail.com>
2026-07-01 17:12:44 +0100

    importer la controller

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
