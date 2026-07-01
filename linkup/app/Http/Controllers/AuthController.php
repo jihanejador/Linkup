b34e074c4ade00d22ef18e524f4c26fc077a575d
jihanejador <jihanejador@gmail.com>
2026-07-01 17:38:25 +0100

    creation la validation de l'email et mot de passe de login

--- linkup/app/Http/Controllers/AuthController.php
+++ linkup/app/Http/Controllers/AuthController.php
@@ -35,6 +35,9 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
-        
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
     }
 }
