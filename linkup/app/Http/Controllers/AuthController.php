183597b58dd2d5a4ef69c63f26fba3ae22799d23
jihanejador <jihanejador@gmail.com>
2026-07-03 09:52:09 +0100

    proteger sur faire csrf

--- linkup/app/Http/Controllers/AuthController.php
+++ linkup/app/Http/Controllers/AuthController.php
@@ -51,5 +51,6 @@ class AuthController extends Controller
     public function logout(Request $request){
         Auth::logout();
         $request->session()->invalidate();
+        $request->session()->regenerateToken();
     }
 }
