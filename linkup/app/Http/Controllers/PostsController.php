2439d8fd337b1460ee99df1007ab58c981d6651a
jihanejador <jihanejador@gmail.com>
2026-07-03 10:34:42 +0100

    importer l

--- linkup/app/Http/Controllers/PostsController.php
+++ linkup/app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Posts;
 use App\Http\Requests\StorePostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
