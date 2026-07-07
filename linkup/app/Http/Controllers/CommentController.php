88b13e2f20323c5ab76729c6cfe98825b5407cdc
jihanejador <jihanejador@gmail.com>
2026-07-07 15:04:34 +0100

    importer les fichiers qui utilise

--- linkup/app/Http/Controllers/CommentController.php
+++ linkup/app/Http/Controllers/CommentController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Posts;
+use App\Models\Comment;
+use Illuminate\Support\Facedes\Auth;
 
 class CommentController extends Controller
 {
