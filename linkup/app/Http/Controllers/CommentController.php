9e2077ca878a00c0cf9804d61b9c7b8c0de56e4a
jihanejador <jihanejador@gmail.com>
2026-07-08 18:43:02 +0100

    ajouter destroy

--- linkup/app/Http/Controllers/CommentController.php
+++ linkup/app/Http/Controllers/CommentController.php
@@ -9,15 +9,26 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    public function store(Request $request, Posts $post){
+    public function store(Request $request, Posts $posts) { // 👈 البارامتر خاصو يكون $posts بحال الـ Route
         $validated = $request->validate([
             'content'=> 'required|string|max:500',
         ]);
+
         Comment::create([
             'user_id' => Auth::id(),
-            'post_id' => $post->id,
+            'post_id' => $posts->id,
             'content' => $validated['content'],
         ]);
-        return back()->with('success', 'Commentaire ajoute avec succes !');
+
+        return back()->with('success', 'Commentaire ajouté avec succès !');
+    }
+
+    public function destroy(Comment $comment) {
+        if (Auth::id() !== $comment->user_id) {
+            abort(403, 'Action non autorisée.');
+        }
+
+        $comment->delete();
+        return back()->with('success', 'Commentaire supprimé avec succès !');
     }
 }
