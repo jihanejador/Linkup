c99865d614abeb668e85f1d883f465174a34a643
jihanejador <jihanejador@gmail.com>
2026-07-08 19:13:07 +0100

    liee entre le post et le commentaire

--- linkup/app/Models/Comment.php
+++ linkup/app/Models/Comment.php
@@ -14,4 +14,7 @@ class Comment extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function post(){
+        return $this->belongsTo(Posts::class, 'post_id');
+    }
 }
