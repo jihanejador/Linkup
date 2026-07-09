49a2602af4a794d42822e57f200d5fa24562fce6
jihanejador <jihanejador@gmail.com>
2026-07-09 11:00:13 +0100

    modifier le typo

--- linkup/database/migrations/2026_07_09_092815_create_likes_table.php
+++ linkup/database/migrations/2026_07_09_092815_create_likes_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->foreingId('user_id')->constrained()->onDelete('cascade');
-            $table_>foreingId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
             $table->timestamps();
             $table->unique(['user_id', 'post_id']);
         });
