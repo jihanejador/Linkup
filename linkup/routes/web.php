bcbfa66ed5e34fe0b9942d4e05e32311e76d1937
jihanejador <jihanejador@gmail.com>
2026-07-09 11:46:35 +0100

    importer les packeges qui utilise

--- linkup/routes/web.php
+++ linkup/routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CommentController; 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 
 Route::get('/', function () {
     return view('welcome');
