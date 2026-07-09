9b7f6d0b86f4c57074bd4d602e8ccea0d4f0530a
jihanejador <jihanejador@gmail.com>
2026-07-09 10:57:05 +0100

    ajouter les attrubes de table like

--- linkup/database/migrations/2026_07_09_092815_create_likes_table.php
+++ linkup/database/migrations/2026_07_09_092815_create_likes_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
+            $table->foreingId('user_id')->constrained()->onDelete('cascade');
+            $table_>foreingId('post_id')->constrained('posts')->onDelete('cascade');
             $table->timestamps();
+            $table->unique(['user_id', 'post_id']);
         });
     }
 
