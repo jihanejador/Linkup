5280b470486077d78e714d6ce8333e8a2e224f2f
jihanejador <jihanejador@gmail.com>
2026-07-10 19:44:36 +0100

    creation la function de virfier est ce que cette perssene abonne ou non

--- linkup/app/Models/User.php
+++ linkup/app/Models/User.php
@@ -52,8 +52,11 @@ class User extends Authenticatable
     public function followings(){
         return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_id');
     }
-    public function followers()
-{
-    return $this->belongsToMany(User::class, 'follows', 'following_id', 'user_id');
-}
+    public function followers(){
+        return $this->belongsToMany(User::class, 'follows', 'following_id', 'user_id');
+    }
+    public function isFollowing(User $user){
+        return $this->followings()->where('following_id', $user->id)->exists();
+    }
+
 }
