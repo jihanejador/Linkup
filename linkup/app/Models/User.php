cfa356919ab6fbadf2dfb5eacc9e6b5b62dc3504
jihanejador <jihanejador@gmail.com>
2026-07-10 19:42:52 +0100

    faire la relation entre user et mes abonnes

--- linkup/app/Models/User.php
+++ linkup/app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
     public function followings(){
         return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_id');
     }
+    public function followers()
+{
+    return $this->belongsToMany(User::class, 'follows', 'following_id', 'user_id');
+}
 }
