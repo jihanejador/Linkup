e3f83bb2d14ffea4e52a4d949023fbc355bcbdce
jihanejador <jihanejador@gmail.com>
2026-07-10 19:35:27 +0100

    changer la function up

--- linkup/database/migrations/2026_07_10_183245_create_follows_table.php
+++ linkup/database/migrations/2026_07_10_183245_create_follows_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('following_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+            $table->unique(['user_id', 'following_id']);
         });
     }
 
