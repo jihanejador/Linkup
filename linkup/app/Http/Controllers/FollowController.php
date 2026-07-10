51bf064e56fdacd447f8f1992ac717341b29e0ca
jihanejador <jihanejador@gmail.com>
2026-07-10 19:48:00 +0100

    faire la securite de proteger le user pas faire abonne de leur compte

--- linkup/app/Http/Controllers/FollowController.php
+++ linkup/app/Http/Controllers/FollowController.php
@@ -8,5 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class FollowController extends Controller
 {
-    //
+    public function toggle(User $user)
+    {
+        $me = Auth::user();
+
+        if ($me->id === $user->id) {
+            return redirect()->back()->with('error', "Vous ne pouvez pas vous suivre vous-même.");
+        }
+
+    }
 }
